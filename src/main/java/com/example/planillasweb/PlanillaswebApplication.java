e36c3249a185cd92e00f3d92d669a622a1d6acc5
Jordan <jordantrejo45@gmail.com>
2025-10-13 18:11:37 -0500

    Initialize Spring Boot project with Maven wrapper and basic structure

---
+++ src/main/java/com/example/planillasweb/PlanillaswebApplication.java
@@ -0,0 +1,13 @@
+package com.example.planillasweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanillaswebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanillaswebApplication.class, args);
+	}
+
+}
