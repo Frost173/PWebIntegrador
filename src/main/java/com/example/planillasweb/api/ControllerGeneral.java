d7addbc21e4afad3cfb79cbcf240a2eb93296653
Snowru <melds3101@gmail.com>
2025-10-14 22:33:40 -0500

    Cambios en el Controlador General

--- src/main/java/com/example/planillasweb/api/ControllerGeneral.java
+++ src/main/java/com/example/planillasweb/api/ControllerGeneral.java
@@ -1,5 +1,59 @@
 package com.example.planillasweb.api;
 
-public class ControllerGeneral {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class ControllerGeneral {
+    @GetMapping("/")
+    public String login(){
+        return "pages/login";
+    }
+
+    @GetMapping("/inicio")
+    public String inicio(){
+        return "pages/index";
+    }
+
+
+    @GetMapping("/usuarios")
+    public String usuarios(){
+        return "pages/usuarios";
+    }
+    @GetMapping("/empresas")
+    public String proyectos(){
+        return "pages/empresas";
+    }
+    @GetMapping("/areas")
+    public String areas(){
+        return "pages/areas";
+    }
+    @GetMapping("/cargos")
+    public String cargos(){
+        return "pages/cargos";
+    }
+    @GetMapping("/empleados")
+    public String empleados(){
+        return "pages/empleados";
+    }
+    @GetMapping("/asistencias")
+    public String asistencias(){
+        return "pages/asistencias";
+    }
+     @GetMapping("/planillas")
+    public String planillas(){
+        return "pages/planillas";
+    }
+    @GetMapping("/detallePlanillas")
+    public String detallePlanillas(){
+        return "pages/detallePlanillas";
+    }
+    @GetMapping("/pagos")
+    public String pagos(){
+        return "pages/pagos";
+    }
+    @GetMapping("/reportes")
+    public String reportes(){
+        return "pages/reportes";
+    }
 }
